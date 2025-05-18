131bf7a14b94c9f4eb6bf97347bc240389d62aa6
Gabi Ferraz <ferrazmgabi@gmail.com>
2025-05-18 18:35:30 -0300

    self review

--- src/main/java/com/api/stock/entrypoint/consumer/RabbitMQEventConsumer.java
+++ src/main/java/com/api/stock/entrypoint/consumer/RabbitMQEventConsumer.java
@@ -7,11 +7,13 @@ import com.api.stock.event.ReleaseStockEvent;
 import com.api.stock.event.ReserveStockEvent;
 import com.api.stock.event.StockReservedEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class RabbitMQEventConsumer {
 
   private final StockService stockService;
@@ -19,9 +21,15 @@ public class RabbitMQEventConsumer {
 
   @RabbitListener(queues = RabbitMQConfig.RESERVE_STOCK_QUEUE)
   public void consumeReserveStockEvent(final ReserveStockEvent event) {
+    log.info(
+        "Processing ReserveStockEvent for productSku: {}, orderId: {}",
+        event.productSku(),
+        event.orderId());
     boolean success =
         this.stockService.reserveStock(event.productSku(), event.quantity(), event.orderId());
 
+    log.info(
+        "Publishing StockReservedEvent for orderId: {}, success: {}", event.orderId(), success);
     this.eventPublisher.publish(new StockReservedEvent(event.orderId(), success));
   }
 
